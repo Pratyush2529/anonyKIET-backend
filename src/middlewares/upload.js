476daa4cf907e701e04a1414f7ef7e28a240042e
Pratyush Sharma <pratyushsharma25feb@gmail.com>
2025-12-20 18:18:53 +0530

    moved multer from disk storage to memory storage

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,16 +1,7 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./public/temp/dp");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
-});
-
 const upload = multer({
-  storage,
+  storage:multer.memoryStorage(),
   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
   fileFilter(req, file, cb) {
     if (!file.mimetype.startsWith("image/")) {
